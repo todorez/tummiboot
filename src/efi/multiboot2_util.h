b6444111c73d9bb8bbfcc1d46aae03435937be6b
Todor Minchev <todor.minchev@intel.com>
2014-08-15 16:24:06 +0100

    tummiboot: add multiboot2 header option to the bootloader
    read multiboot header from the kernel and parse the header tags
    
    Signed-off-by: Todor Minchev <todor.minchev@intel.com>

---
+++ src/efi/multiboot2_util.h
@@ -0,0 +1,42 @@
+#ifndef __MULTIBOOT2_UTIL_H
+#define __MULTIBOOT2_UTIL_H
+
+#include <efi.h>
+#include <efilib.h>
+#include "multiboot2.h"
+
+#define MULTIBOOT_OS_CONSOLE_EGA_TEXT 1
+#define MULTIBOOT_CONSOLE_FRAMEBUFFER 2
+
+#define ALIGN_UP(addr, align) \
+        ((addr + (typeof (addr)) align - 1) & ~((typeof (addr)) align - 1))
+
+typedef enum { false, true } bool;
+
+typedef struct multiboot_header mboot_hdr_t ;
+typedef mboot_hdr_t* mboot_hdr_p ;
+
+typedef struct multiboot_header_tag mboot_hdr_tag_t ;
+typedef mboot_hdr_tag_t* mboot_hdr_tag_p ;
+
+typedef struct multiboot_header_tag_information_request mboot_hdr_tag_info_req_t ;
+typedef mboot_hdr_tag_info_req_t* mboot_hdr_tag_info_req_p ;
+
+typedef struct multiboot_header_tag_address mboot_hdr_tag_addr_t ;
+typedef mboot_hdr_tag_addr_t* mboot_hdr_tag_addr_p ;
+
+typedef struct multiboot_header_tag_entry_address mboot_hdr_tag_entry_addr_t ;
+typedef mboot_hdr_tag_entry_addr_t* mboot_hdr_tag_entry_addr_p ;
+
+typedef struct multiboot_header_tag_console_flags mboot_hdr_tag_con_flags_t ;
+typedef mboot_hdr_tag_con_flags_t* mboot_hdr_tag_con_flags_p ;
+
+
+typedef struct multiboot_header_tag_framebuffer mboot_hdr_tag_fbuf_t ;
+typedef mboot_hdr_tag_fbuf_t* mboot_hdr_tag_fbuf_p ;
+
+EFI_STATUS copy_header_buf(EFI_HANDLE parent_image, CHAR16 *mboot_file, CHAR8 **buf, UINTN *mboot_len) ;
+EFI_STATUS parse_header(CHAR8 *buf, UINTN len) ;
+
+
+#endif
