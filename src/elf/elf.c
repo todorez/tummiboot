e85e018d6d3e2d55692a64224594655a8f5c9db4
Todor Minchev <todor.minchev@intel.com>
2014-09-15 16:42:14 +0100

    gummiboot: add ACPI v1/v2 support
    Signed-off-by: Todor Minchev <todor.minchev@intel.com>

--- src/elf/elf.c
+++ src/elf/elf.c
@@ -2,6 +2,8 @@
 #include <efilib.h>
 #include "elf.h"
 
+extern VOID *mbi2_buf ;
+
 static inline void *memset(void *b, int c, uint32_t len)
 {
 	char *bb;
@@ -23,7 +25,7 @@ EFI_STATUS load_elf(CHAR8 *buf, void **entry)
         uefi_call_wrapper(BS->Stall, 1, 1 * 1000 * 1000);
         return EFI_LOAD_ERROR;
     }else
-    	elf = (elf_program_header_t *) buf ;
+    	elf = (elf_header_t *) buf ;
 
     /* validate the ELF header */
     if (elf->e_ident[EI_MAG0] != ELFMAG0
@@ -55,28 +57,46 @@ EFI_STATUS load_elf(CHAR8 *buf, void **entry)
 				 ((void *)elf + elf->e_phoff + i*elf->e_phentsize);
 
         if ( ph->p_type == PT_LOAD ) {
-            memcpy((void *)ph->p_paddr, (void *)elf + ph->p_offset,
+            memcpy((void *)(uint64_t)ph->p_paddr, (void *)elf + ph->p_offset,
                    ph->p_filesz);
-            memset((void *)(ph->p_paddr + ph->p_filesz), 0,
+            memset((void *)(uint64_t)(ph->p_paddr + ph->p_filesz), 0,
                    ph->p_memsz - ph->p_filesz);
         }
     }
 
-    *entry = (void*) elf->e_entry;
+    *entry = (void*) (uint64_t)elf->e_entry;
 
 
     return EFI_SUCCESS;
 }
 
-EFI_STATUS start_elf(void *buf){
-    /* EAX - multiboot2 magic, EBX - MBI2, ECX - entry point */
-    Print(L"elf.c : %d: Entry address is %x.\n", __LINE__, buf);
-    uefi_call_wrapper(BS->Stall, 1, 1 * 1000 * 1000);
+void start_elf(void *buf){
 
+    if(!buf){
+    		Print(L"elf.c : %d : Missing ELF entry point. Resetting.\n", __LINE__);
+    		uefi_call_wrapper(BS->Stall, 1, 3 * 1000 * 1000);
+    		uefi_call_wrapper(RT->ResetSystem, 4, EfiResetCold, EFI_SUCCESS,0,0);
+    }
+
+    if(!mbi2_buf){
+    		Print(L"elf.c : %d : Missing MBI2 buffer. Resetting.\n", __LINE__);
+    		uefi_call_wrapper(BS->Stall, 1, 3 * 1000 * 1000);
+    		uefi_call_wrapper(RT->ResetSystem, 4, EfiResetCold, EFI_SUCCESS,0,0);
+
+    }
+
+    Print(L"elf.c : %d: MAGIC 			%x.\n", __LINE__, MULTIBOOT2_BOOTLOADER_MAGIC);
+    Print(L"elf.c : %d: MBI2 ADDRESS 	%x.\n", __LINE__, mbi2_buf);
+    Print(L"elf.c : %d: ENTRY ADDRESS 	%x.\n\n", __LINE__, buf);
+    Print(L"elf.c : %d: LAUNCHING\n", __LINE__);
+
+    uefi_call_wrapper(BS->Stall, 1, 3 * 1000 * 1000);
+
+    /* EAX - multiboot2 magic, EBX - MBI2, ECX - entry point */
     __asm__ __volatile__ (
           "    jmp *%%rcx;    "
           "    ud2;          "
-          :: "a" (MULTIBOOT2_BOOTLOADER_MAGIC), "b" (NULL), "c" (buf));
+          :: "a" (MULTIBOOT2_BOOTLOADER_MAGIC), "b" (mbi2_buf), "c" (buf));
 
 
 }
