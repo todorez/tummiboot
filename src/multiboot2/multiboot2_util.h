d18a99e972db79a212aa069f4a066b019e512c18
Todor Minchev <todor.minchev@intel.com>
2014-10-17 17:29:25 +0100

    tummiboot: return the CPU to protected mode before tboot handover
    implements a GDT with CS32, DS32, CS16 and DS16 segment descriptors
    switches the CPU operating mode to protected mode (via compatibility)
    hands over execution to tboot
    
    Signed-off-by: Todor Minchev <todor.minchev@intel.com>

--- src/multiboot2/multiboot2_util.h
+++ src/multiboot2/multiboot2_util.h
@@ -14,9 +14,13 @@
 #define E820_EXEC_CODE  5
 
 
-
 #define MULTIBOOT_OS_CONSOLE_EGA_TEXT 1
 #define MULTIBOOT_CONSOLE_FRAMEBUFFER 2
+
+#define cs_sel      1<<3
+#define ds_sel      2<<3
+#define gdt_cs_flags_limit  0x9a /* present, system, DPL-0, execute/read     */
+#define gdt_ds_flags_limit  0x92 /* present, system, DPL-0, read/write       */
 
 #define ALIGN_UP(addr, align) \
         ((addr + (typeof (addr)) align - 1) & ~((typeof (addr)) align - 1))
@@ -89,6 +93,42 @@ typedef struct{
 
 }fb_rgbr_mask_field_t ;
 
+struct seg_desc {
+
+    uint16_t limit_15_0;
+    uint16_t base_addr_15_0;
+    uint8_t base_addr_23_16;
+
+    /* 4 bits flags + 4 bits limit*/
+    uint8_t flags_lim;
+
+    /* Bits 16-19 in the segment limiter. */
+    uint8_t limit_19_16:4;
+
+    uint8_t u:1;
+    uint8_t x:1;
+
+    /*  D=0 16-bit segment, D=1, 32-bit */
+    uint8_t d:1;
+
+    /* Granularity G=0 1 byte, G=1 4KB */
+    uint8_t g:1;
+    uint8_t base_addr_31_24;
+} __attribute__((__packed__));
+
+/* Build a 4KB granular segment descriptor. */
+#define populate_4k_seg_descriptor(base_addr, limit, bits, flags_limit) ((struct seg_desc) {	\
+        .limit_15_0 = ((limit) >> 12) & 0xffff,                                                 \
+        .limit_19_16 = ((limit) >> 28) & 0xf,							\
+        .base_addr_15_0 = (base_addr) & 0xffff,							\
+        .base_addr_23_16 = ((base_addr) >> 16) & 0xff,						\
+        .base_addr_31_24 = ((base_addr) >> 24) & 0xff,						\
+        .u = 0,											\
+        .x = 0,											\
+        .d = (bits),										\
+        .g = 1,											\
+        .flags_lim = (flags_limit)})
+
 typedef enum { false, true } bool;
 
 typedef struct multiboot_header mboot_hdr_t ;
@@ -116,8 +156,8 @@ typedef mboot_hdr_tag_fbuf_t* mboot_hdr_tag_fbuf_p ;
 EFI_STATUS copy_file_buf(EFI_HANDLE parent_image, CHAR16 *mboot_file, CHAR8 **buf, UINTN *mboot_len) ;
 EFI_STATUS parse_header(CHAR8 *buf, UINTN len) ;
 EFI_STATUS load_elf(CHAR8 *buf, void **entry) ;
-void start_elf(void *buf) ;
-EFI_STATUS populate_mbi2(EFI_HANDLE parent_image, const ConfigEntry *entry) ;
+void start_elf(void *buf, void* mbi2_buf) ;
+EFI_STATUS populate_mbi2(EFI_HANDLE parent_image, const ConfigEntry *entry, void** mbi2_buf) ;
 void *memcpy(void *dst0, const void *src0, unsigned long length) ;
 int memcmp (const void *s1, const void *s2, unsigned n) ;
 
